756fc2419932deed253f6958864c0aeab670fcd3
wei.yang <geekstylecn@gmail.com>
2018-11-01 15:58:21 +0800

    modify deploy script

--- postbuild.sh
+++ postbuild.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # build image, upload image, update service image
 
-docker build -t geekstylecn/survey:1.0.0 .
-docker push geekstylecn/survey:1.0.0
-docker service update survey --image geekstylecn/survey:1.0.0
+#docker build -t geekstylecn/survey:1.0.0 .
+#docker push geekstylecn/survey:1.0.0
+#docker service update survey --image geekstylecn/survey:1.0.0
+#echo "---done---"
+
+mv ./build/libs/*.jar /server/survey/survey.jar
+java -jar /server/survey/survey.jar
 echo "---done---"

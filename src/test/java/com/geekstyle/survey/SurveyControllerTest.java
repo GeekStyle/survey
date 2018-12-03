69b576753ac0b224503237dd9a5d2754a7343c6f
wei.yang <geekstylecn@gmail.com>
2018-12-03 17:07:16 +0800

    modify test to http get method

--- src/test/java/com/geekstyle/survey/SurveyControllerTest.java
+++ src/test/java/com/geekstyle/survey/SurveyControllerTest.java
@@ -29,7 +29,7 @@ public class SurveyControllerTest {
     public void testInsertSurvey() throws Exception {
         Survey survey = new Survey();
         survey.setName("John");
-        String result = restTemplate.postForEntity("http://localhost:" + port + "/survey", survey, HashMap.class).toString();
+        String result = restTemplate.getForObject("http://localhost:" + port + "/survey/1", HashMap.class).toString();
         System.out.println(result);
         assertThat(result).contains("John");
     }
